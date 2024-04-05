b20882b02aff8a63c6545c7e535d5ff74f56e435
ggbiktashev <ggbiktashev@avito.ru>
2024-04-05 15:20:28 +0500

    Move all libraries into libs folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,12 +8,13 @@ dependencyResolutionManagement {
 
 rootProject.name = "time-test"
 
-include(
-    "core",
-    "core-test",
-    "plain",
-    "mockito",
-    "junit5-integration"
-)
+val filesWithinLibsDirectory: Array<File> = file("libs").listFiles() ?: arrayOf()
+
+filesWithinLibsDirectory.filter { it.isDirectory }
+    .map { it.name }
+    .forEach {
+        include(it)
+        project(":$it").projectDir = file("libs/$it")
+    }
 
 include("platform")
